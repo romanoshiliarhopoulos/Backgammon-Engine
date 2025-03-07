3845e24c42683ef5fea0e9fec06525b96866c932
Romanos Chiliarchopoulos <romanoshiliarhopoulos@gmail.com>
2025-03-06 23:46:58 -0600

    new

--- Pieces.cpp
+++ Pieces.cpp
@@ -5,8 +5,10 @@
 // constructor
 Pieces::Pieces()
 {
-    this->numPieces_p1 = 0;
+    this->numPieces_p1 = 0; // number of jailed pieces
     this->numPieces_p2 = 0;
+    this->freedPieces_p1 = 0; // number of 'freed' pieces
+    this->freedPieces_p2 = 0;
 }
 
 /// @brief returns true if the current player has a jailed piece
@@ -28,10 +30,54 @@ bool Pieces::hasJailedPiece(int player)
 /// @param player player 1 or player 0
 void Pieces::addJailedPiece(int player)
 {
+    if (player == Player::PLAYER1)
+    {
+        this->numPieces_p1 += 1;
+    }
+    else
+    {
+        this->numPieces_p2 += 1;
+    }
 }
 
 /// @brief removes a jailedpiece
 /// @param player player 1 or player 0
 void Pieces::removeJailedPiece(int player)
 {
+    if (player == Player::PLAYER1 && this->numPieces_p1 > 0)
+    {
+        this->numPieces_p1 -= 1;
+    }
+    else
+    {
+        this->numPieces_p2 -= 1;
+    }
+}
+
+/// @param player enum (either 1 or 0)
+/// @return returns the number of freed pieces based on given player
+int Pieces::numFreed(int player)
+{
+    if (player == Player)
+    {
+        return this->freedPieces_p1;
+    }
+    else
+    {
+        return this->numPieces_p2;
+    }
+}
+
+/// @brief Adds one more to the number of freed pieces to the given player
+/// @param player enum, either 0 or 1
+void Pieces::freePiece(int player)
+{
+    if (player == Player::PLAYER1)
+    {
+        this->freedPieces_p1 += 1;
+    }
+    else
+    {
+        this->freedPieces_p2 += 1;
+    }
 }
