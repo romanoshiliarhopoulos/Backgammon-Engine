29cf7b354766385310b9152180bf85986cdef7c3
Romanos Chiliarchopoulos <romanoshiliarhopoulos@gmail.com>
2025-05-16 21:35:01 -0500

    n

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/romanos/Backgammon_Engine
-# Build directory: /Users/romanos/Backgammon_Engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/romanos/Backgammon_Engine/build/backgammon_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
